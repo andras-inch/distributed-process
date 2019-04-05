d50ac916850300ec0f5334dd5ff4faae4037a06d
Andras Horvath <andras.horvath@inchora.com>
2019-04-05 16:43:20 +0100

    chanes to test app

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,40 @@
 module Main where
-import Network.Transport.TCP (createTransport, defaultTCPParameters, defaultTCPAddr)
+
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
+import Data.Map (keys)
+import Network.Transport.TCP
+  ( createTransport
+  , defaultTCPAddr
+  , defaultTCPParameters
+  )
+-- import System.IO
+import System.Environment
 
 main :: IO ()
 main = do
-  Right t <- createTransport (defaultTCPAddr "127.0.0.1" "10501") defaultTCPParameters
-  node <- newLocalNode t initRemoteTable
-  _ <- runProcess node $ do
-    stats <- getLocalNodeStats
-    liftIO $ print stats
-    s <- getLocalRegistry
-    liftIO $ print s
-  return ()
+  args <- getArgs
+  let port = case args of
+               [] -> defaultPort
+               [p] -> read p
+               _ -> error "???"
+  runLocalNode port $ do
+     -- getSelfPid >>= say . show
+     -- liftIO $ hFlush stderr
+     stats <- getLocalNodeStats
+     liftIO $ print stats
+     liftIO . print =<< registered
+
+registered :: Process [String]
+registered = keys <$> getLocalRegistry
+
+defaultPort :: Int
+defaultPort = 9999
+
+runLocalNode :: Int -> Process () -> IO ()
+runLocalNode port action = do
+  Right t <- createTransport
+        (defaultTCPAddr "127.0.0.1" $ show port)
+        defaultTCPParameters
+  n <- newLocalNode t initRemoteTable
+  runProcess n action
